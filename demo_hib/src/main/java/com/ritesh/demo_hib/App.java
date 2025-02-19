ca2058b7e85ea4c46ca837557cff61134a9bf30f
Ritesh Pandey <ritesh2897pandey@gmail.com>
2025-02-19 14:59:13 +0530

    update

--- demo_hib/src/main/java/com/ritesh/demo_hib/App.java
+++ demo_hib/src/main/java/com/ritesh/demo_hib/App.java
@@ -1,5 +1,10 @@
 package com.ritesh.demo_hib;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Student s1 = new Student();
+        s1.setId(1002);
+        s1.setName("anmol");
+        s1.setTech("java_script");
+
+
+        Configuration config = new Configuration();
+        config.addAnnotatedClass(com.ritesh.demo_hib.Student.class);
+        config.configure("hibernate.cfg.xml");
+
+        SessionFactory factory = config.buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(s1);
+        transaction.commit();
     }
 }
+
